42f8f58abbca386cf8837b9d5c5d17f6fa862535
Dani Issac <daniissac654321@gmail.com>
2024-10-14 00:15:07 +0530

    Prepare for GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/whisperlink/',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/manifest.json', to: 'manifest.json' },
+                { from: 'src/.nojekyll', to: '.nojekyll' },
+                // Add this line if you have an icon.png file
+                // { from: 'src/icon.png', to: 'icon.png' },
+            ],
         }),
     ],
     devServer: {
